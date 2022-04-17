41559b9e9ec25cefcf63776625d3c352c37c2833
BernardoEspinoza02 <berespinozan@outlook.com>
2022-04-17 13:19:01 -0500

    example5

--- weekly_mission_2/example/example5/pokemon.test.js
+++ weekly_mission_2/example/example5/pokemon.test.js
@@ -8,4 +8,3 @@ test('1) Create a new object pokemon', () => {
 =======
     expect(myPokemon.name).toBe('Pikachussssss');//Corrige esta prueba 
 });
->>>>>>> 7eb8bbc1272082633a8c1248b19b00f378b46a87
