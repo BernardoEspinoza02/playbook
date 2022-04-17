7eb8bbc1272082633a8c1248b19b00f378b46a87
Bernardo_Espinoza <97721727+BernardoEspinoza02@users.noreply.github.com>
2022-04-17 13:03:00 -0500

    prueba5

--- weekly_mission_2/example/example5/pokemon.test.js
+++ weekly_mission_2/example/example5/pokemon.test.js
@@ -2,5 +2,5 @@ import Pokemon from './pokemon.js'
 
 test('1) Create a new object pokemon', () => {
     const myPokemon = new Pokemon('Pikachu')
-    expect(myPokemon.name).toBe('Pikachussssss');//Corrige esta prueba   
+    expect(myPokemon.name).toBe('Pikachussssss');//Corrige esta prueba 
 });
