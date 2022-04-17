29713b539d0568981a3171be77e43962e5bdf329
BernardoEspinoza02 <berespinozan@outlook.com>
2022-04-17 13:27:35 -0500

    cambio

--- weekly_mission_2/example/example5/pokemon.test.js
+++ weekly_mission_2/example/example5/pokemon.test.js
@@ -2,9 +2,7 @@ import Pokemon from './pokemon.js'
 
 test('1) Create a new object pokemon', () => {
     const myPokemon = new Pokemon('Pikachu')
-<<<<<<< HEAD
     expect(myPokemon.name).toBe('Pikachu');//Corrige esta prueba  
 });
-=======
     expect(myPokemon.name).toBe('Pikachussssss');//Corrige esta prueba 
 });
