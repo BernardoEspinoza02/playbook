1ed5908c8fb3341ce795cf6cd4d33d0a747d3da6
BernardoEspinoza02 <berespinozan@outlook.com>
2022-04-17 21:17:09 -0500

    PullRequest

--- weekly_mission_2/exercises/ejercicio_1/pull.js
+++ weekly_mission_2/exercises/ejercicio_1/pull.js
@@ -1,4 +1,21 @@
 const pullrequest = {
-    
+    title:"",
+    branchName:"",
+    dateCreated:"",
+    status:"",
+    repositoryNameAssociated:"",
+    getStatus: function(){
+        return `El status del pullreques es: ${this.status}`
+    },
+    getTitleAndAutor: function(){
+        return this.title + this.repositoryNameAssociated
+
+    }
 
 }
+
+console.log("Titulo: ", pullrequest.title)
+console.log("Branch Name: ", pullrequest.branchName)
+console.log("Día creado: ", pullrequest.dateCreated)
+console.log(pullrequest.getStatus())
+console.log(pullrequest.getTitleAndAutor())
