a7e922785d52664c8c33cbc4af318e3c20da289f
BernardoEspinoza02 <berespinozan@outlook.com>
2022-04-20 00:23:49 -0500

    Prueba de Error

--- weekly_mission_3/my_launchx_app/test/missionCommander.test.js
+++ weekly_mission_3/my_launchx_app/test/missionCommander.test.js
@@ -1,8 +1,13 @@
-const { TestWatcher } = require("jest")
+const MissionCommander = require ('./../app/missionCommander');
 
-describe("Esto es una suite de pruebas", () => {
-    test ('Caso de prueba 1', () => {
-        const result = 1 + 2
-        expect (result).toBe(3);
+describe("Unit Tests for Mission Commander Class", () => {
+    test ('1) Create a mission commander objet', () =>{
+    //test ('Caso de prueba 1', () => {
+        //Aqui puedes usar el codigo como lo deseas utilizar 
+        //const result = 1 + 2
+        const myMissioncomander = new MissionCommander("Woopa")
+
+        //Validar el resultado esperado 
+        expect (myMissioncomander.name).toBe("Woopaa");
     });
 })
